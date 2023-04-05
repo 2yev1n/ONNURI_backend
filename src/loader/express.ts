d2254c8e15382762b1b5f012d39f851b048b6d20
yebin <yebin5127@gmail.com>
2023-04-05 17:11:46 +0900

    Create express loader

---
+++ src/loader/express.ts
@@ -0,0 +1,39 @@
+import fs from "fs";
+import express, { Application, NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { NotFoundURLError } from "../shared/exception";
+
+const PORT = process.env.PORT || 3000;
+
+export const loadExpress = (app: Application) => {
+    app.set('port', PORT);
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        express.json()(req, res, next);
+    });
+
+    app.use(morgan('dev'));
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        express.urlencoded({ extended: false })(req, res, next)
+    });
+
+    const CorsOption = {
+        origin: '*',
+        method: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD'],
+		credentials: true,
+	};
+
+    app.use(cors(CorsOption));
+
+    // app.use('/');
+
+    app.use((req: Request, res: Response, next: NextFunction) => {
+        next(new NotFoundURLError(req.url));
+    });
+
+    app.listen(app.get('port'), () => {
+        console.log(PORT, "포트에서 대기 중");
+    });
+}
