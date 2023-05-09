4321a3e592df5de90d014b0a479b042611edfbc1
yebin <yebin5127@gmail.com>
2023-05-09 20:15:30 +0900

    Update: entities 불러오기

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -3,8 +3,10 @@ import { ConnectionOptions } from 'typeorm';
 
 import { User } from '../entity/user.entity';
 import { Apt } from '../entity/apt.entity';
+import { Like } from '../entity/like.entity';
 import { Post } from '../entity/post.entity';
 import { Notice } from '../entity/notice.entity';
+import { Comment } from '../entity/comment.entity';
 
 export const createOptions: ConnectionOptions = {
 	type: 'mysql',
@@ -13,9 +15,9 @@ export const createOptions: ConnectionOptions = {
 	username: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
-	synchronize: true,
-	logging: true,
+	synchronize: false,
+	logging: false,
 	timezone: 'Z',
-	entities: ['src/entity/*.ts'],
+	entities: [Post, User, Apt, Comment, Like, Notice],
 	charset: 'utf8mb4' 
 };
