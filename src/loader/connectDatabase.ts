90fa8816e76b3898b4eb640985a9641d0f24990e
yebin <yebin5127@gmail.com>
2023-05-09 20:17:12 +0900

    Update: 비동기처리 추가

--- src/loader/connectDatabase.ts
+++ src/loader/connectDatabase.ts
@@ -1,10 +1,10 @@
 import { createConnection } from 'typeorm';
 import { createOptions } from '../config/ormconfig';
 
-export const connectDatabase = () => {
-		createConnection(createOptions)
-			.then(() => console.log('데이터베이스 연결 성공'))
-			.catch(err => {
-				console.error(err);
-			});
+export const connectDatabase = async () => {
+	await createConnection(createOptions)
+		.then(() => console.log('데이터베이스 연결 성공'))
+		.catch(err => {
+			console.error(err);
+		});
 };
