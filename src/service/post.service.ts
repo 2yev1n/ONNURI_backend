aa70311950e1a71f4f2c1e791a2a7f90c5f79571
yebin <yebin5127@gmail.com>
2023-05-10 09:52:48 +0900

    Update: post 기능 로직 추가

--- src/service/post.service.ts
+++ src/service/post.service.ts
@@ -1,14 +1,31 @@
 import { User } from "../entity/user.entity";
 import { PostRepository } from "../repository/post.repository";
-import { CreatePostInfo, PostInfo } from "../shared/DataTransferObject";
+import { EditPostInfo, PostInfo } from "../shared/DataTransferObject";
+import { ForbiddenError } from "../shared/exception";
 
 export class PostService {
     constructor(
         private postRepository: PostRepository
     ) {}
 
-    async createPost(createPostInfo: CreatePostInfo, user: User) {
-        const postInfo: PostInfo = { ...createPostInfo, user_id: user.id, apt_id: user.apt_id };
+    async createPost(editPostInfo: EditPostInfo, user: User) {
+        const postInfo: PostInfo = { ...editPostInfo, user_id: user.id, apt_id: user.apt_id };
         return await this.postRepository.createPost(postInfo);
+    }
+
+    async updatePost(editPostInfo: EditPostInfo, post_id: number, user: User) {
+        const post = await this.postRepository.findOnePost(post_id);
+        
+        if (post?.user_id !== user.id) throw new ForbiddenError();
+        
+        return await this.postRepository.updatePost(post_id, editPostInfo);
+    }
+
+    async findOnePost(post_id: number) {
+        return await this.postRepository.findOnePost(post_id);
+    }
+
+    async findAllPost(apt_id: number) {
+        return await this.postRepository.findAllPost(apt_id);
     }
 }
