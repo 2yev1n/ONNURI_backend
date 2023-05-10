5e07df2f50452495d1b475acf1cb6b215e3e94c5
yebin <yebin5127@gmail.com>
2023-05-10 17:30:37 +0900

    Update: comment router 추가

--- src/route/index.router.ts
+++ src/route/index.router.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { userSerivceRouter } from "./user.router";
 import { postServiceRouter } from "./post.router";
 import { LikeServiceRouter } from "./like.router";
+import { CommentServiceRouter } from "./comment.router";
 
 export const OnnuriRouter = () => {
     const app = Router();
   
     userSerivceRouter(app);
+    CommentServiceRouter(app);
     LikeServiceRouter(app);
     postServiceRouter(app);
 
