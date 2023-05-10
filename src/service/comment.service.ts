1bff5ad2bdf598be2f966422956d9eda66214546
yebin <yebin5127@gmail.com>
2023-05-10 17:30:46 +0900

    Update: 댓글 작성 기능 추가

--- src/service/comment.service.ts
+++ src/service/comment.service.ts
@@ -1,7 +1,16 @@
+import { Comment } from "../entity/comment.entity";
+import { User } from "../entity/user.entity";
 import { CommentRepository } from "../repository/comment.repository";
+import { CommentInfo } from "../shared/DataTransferObject";
 
 export class CommentService {
     constructor(
         private commentRepository: CommentRepository
     ) {}
+
+    async createComment(post_id: number, content: string, user: User): Promise<Comment> {
+        const commentInfo: CommentInfo = { post_id, content, user_id: user.id };
+
+        return await this.commentRepository.createComment(commentInfo);
+    }
 }
