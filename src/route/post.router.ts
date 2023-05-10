8ab3398fe045ef8c8e1340ee860a3c898bea2e89
yebin <yebin5127@gmail.com>
2023-05-10 09:55:53 +0900

    Update: post URI 설정

--- src/route/post.router.ts
+++ src/route/post.router.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { PostController } from "../controller/post.controller";
 import { verifyTokenLogic } from "../middleware/verifyToken";
+import { errorHandler } from "../middleware/errorHandler";
 
 const router: Router  = Router();
 export const postServiceRouter = (app: Router) => {
     const postController: PostController = new PostController();
 
-    app.use('/post', router);
+    app.use('/apt', router);
 
-    router.post('/', verifyTokenLogic, postController.createPost);
+    router.post('/:apt_id/post', verifyTokenLogic, errorHandler(postController.createPost));
+    router.patch('/:apt_id/post/:post_id', verifyTokenLogic, errorHandler(postController.updatePost));
+    router.get('/:apt_id/post/:post_id', verifyTokenLogic, errorHandler(postController.findOnePost));
+    router.get('/:apt_id/post', verifyTokenLogic, errorHandler(postController.findAllPost));
 }
