094f1a5b53c9450817e17c8a0b9108ed37e37f43
yebin <yebin5127@gmail.com>
2023-05-10 09:52:56 +0900

    Update: post 기능 추가

--- src/repository/post.repository.ts
+++ src/repository/post.repository.ts
@@ -1,6 +1,6 @@
 import { EntityRepository, Repository, getCustomRepository } from "typeorm";
 import { Post } from "../entity/post.entity";
-import { CreatePostInfo } from "../shared/DataTransferObject";
+import { EditPostInfo } from "../shared/DataTransferObject";
 
 @EntityRepository(Post)
 export class PostRepository extends Repository<Post> {
@@ -8,9 +8,46 @@ export class PostRepository extends Repository<Post> {
         return getCustomRepository(PostRepository);
     }
 
-    async createPost(createPostInfo: CreatePostInfo): Promise<Post> {
-        const newPost = this.create(createPostInfo);
+    async createPost(editPostInfo: EditPostInfo): Promise<Post> {
+        const newPost = this.create(editPostInfo);
 
         return await this.save(newPost);
+    }
+
+    async updatePost(post_id: number, editPostInfo: EditPostInfo): Promise<Post | undefined> {
+        this.update(post_id, editPostInfo);
+        return await this.findOne(post_id);
+    }
+    
+    async findOnePost(post_id: number): Promise<Post | undefined> {
+        return await this.createQueryBuilder('post')
+            .select('post.id')
+            .addSelect('post.title')
+            .addSelect('post.content')
+            .addSelect('post.apt_id')
+            .addSelect('post.createdAt')
+            .addSelect('post.updatedAt')
+            .addSelect('user.nickname')
+            .addSelect('apt.name')
+            .innerJoin('post.user', 'user')
+            .innerJoin('post.apt', 'apt')
+            .where('post.id = :post_id', { post_id })
+            .getRawOne();
+    }
+
+    async findAllPost(apt_id: number): Promise<Post[]> {
+        return await this.createQueryBuilder('post')
+        .select('post.id')
+        .addSelect('post.title')
+        .addSelect('post.content')
+        .addSelect('post.apt_id')
+        .addSelect('post.createdAt')
+        .addSelect('post.updatedAt')
+        .addSelect('user.nickname')
+        .addSelect('apt.name')
+        .innerJoin('post.user', 'user')
+        .innerJoin('post.apt', 'apt')
+        .where('post.apt_id = :apt_id', { apt_id })
+        .getRawMany();
     }
 }
